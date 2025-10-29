f769aeb03aaf7877359dbc6136d75d2c7d3ab13d
mariadiasss <mariacoelhodias94@gmail.com>
2025-10-29 14:00:26 -0300

    continuação(erro)

--- GMovies/components/trendingMovies.js
+++ GMovies/components/trendingMovies.js
@@ -1,9 +1,21 @@
 import { View, Text, Dimensions, TouchableWithoutFeedback, Image } from 'react-native'
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-//import Carousel from '@react-native-reanimated-carousel';
+import Carousel from '@react-native-reanimated-carousel';
 
 var { width, height} = Dimensions.get('window');
+
+const MovieCard = ({item, handleClick}) => {
+  return (
+  <TouchableWithoutFeedback onPress={() => handleClick(item)}>
+    <Image
+      source={require('../assets/images/moviePoster1.jpg')}
+      style={{width: '100%', height: '100%'}}
+      className="rounded-2xl"
+    />
+  </TouchableWithoutFeedback>
+  )
+}
 
 export default function TrendingMovies({data}) {
     const navigation = useNavigation();
@@ -14,7 +26,21 @@ export default function TrendingMovies({data}) {
   return (
     <View className="mb-8">
       <Text className="text-white text-xl mx-4 mb-3">Em Alta</Text>
-      
+      <Carousel
+        loop
+        width={width * 0.6}
+        height={height * 0.4}
+        autoPlay={false}
+        data={data}
+        renderItem={({item}) => <MovieCard item={item} handleClick={handleClick} />}
+        mode='parallax'
+        modeConfig={{
+          parallaxScrollingScale: 1,
+          parallaxScrollingOffSet: 1,
+          parallaxAdjacentItemScale: 0.9
+        }}
+        style={{ width: width, justifyContent: 'center' }}
+      />
     </View>
   )
 }
