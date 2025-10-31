6f7fb7540ab821e96173b2a882b16b973b699c9d
mariadiasss <mariacoelhodias94@gmail.com>
2025-10-31 12:26:33 -0300

    continuação erro no MovieList

--- GMovies/components/trendingMovies.js
+++ GMovies/components/trendingMovies.js
@@ -1,42 +1,42 @@
-import { View, Text, Dimensions, TouchableWithoutFeedback, Image } from 'react-native'
 import React from 'react';
+import { View, Text, Dimensions, TouchableWithoutFeedback, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Carousel from '@react-native-reanimated-carousel';
+import Carousel from 'react-native-reanimated-carousel';
 
-var { width, height} = Dimensions.get('window');
+var { width, height } = Dimensions.get('window');
 
-const MovieCard = ({item, handleClick}) => {
+const MovieCard = ({item, handleClick }) => {
   return (
   <TouchableWithoutFeedback onPress={() => handleClick(item)}>
     <Image
-      source={require('../assets/images/moviePoster1.jpg')}
-      style={{width: '100%', height: '100%'}}
-      className="rounded-2xl"
+      source= {require('../assets/images/moviePoster1.jpg')}
+      style= {{ width: '100%', height: '100%' }}
+      className= "rounded-2xl"
     />
   </TouchableWithoutFeedback>
   )
 }
+
 
 export default function TrendingMovies({data}) {
     const navigation = useNavigation();
     const handleClick = (item) => {
         navigation.navigate('Movie', item);
     }
-
   return (
     <View className="mb-8">
       <Text className="text-white text-xl mx-4 mb-3">Em Alta</Text>
-      <Carousel
+      <Carousel 
         loop
         width={width * 0.6}
         height={height * 0.4}
-        autoPlay={false}
+        autoPlay= {false}
         data={data}
-        renderItem={({item}) => <MovieCard item={item} handleClick={handleClick} />}
+        renderItem={({item}) => <MovieCard item={item} handleClick={handleClick}/>}
         mode='parallax'
         modeConfig={{
           parallaxScrollingScale: 1,
-          parallaxScrollingOffSet: 1,
+          parallaxScrollingOffset: 1,
           parallaxAdjacentItemScale: 0.9
         }}
         style={{ width: width, justifyContent: 'center' }}
